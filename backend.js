8943c6321ac1ae012227d9759b5cb12550a23b0d
adamfaragoo <adam.farago004@gmail.com>
2021-12-09 16:26:47 +0100

    asd

--- backend.js
+++ backend.js
@@ -3,6 +3,10 @@ const app = express()
 const port = 3000
 var cors = require('cors')
 
+app.use(cors())
+app.use(express.json())
+app.use(express.static(('kepek')))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -39,23 +43,22 @@ app.post('/kereses', (req, res) => {
 })
 
 connection.connect()
-connection.query('SELECT * FROM filmek WHERE filmek.film_ev = "'+req.body.bevitel1+'"', function (err, rows, fields) { 
+let sz='SELECT * from filmek WHERE filmek.film_cim like "%'+req.body.bevitel1+'%"';
+  connection.query(sz, function (err, rows, fields) {
 if (err) throw err
 
   console.log(rows)
-  
   res.send(rows)
 })
 
 connection.end()
 })
 
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static(('kepek')))
+
 
 
