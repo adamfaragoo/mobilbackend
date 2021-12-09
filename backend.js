1cfc57a5d026dcd2ffec51d4c257d80141f044b5
adamfaragoo <adam.farago004@gmail.com>
2021-12-09 13:58:16 +0100

    asd

--- backend.js
+++ backend.js
@@ -28,6 +28,28 @@ connection.query('SELECT * from filmek', function (err, rows, fields) {
 
 connection.end()
 })
+
+app.post('/kereses', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'vizsgamunka'
+})
+
+connection.connect()
+connection.query("SELECT * FROM filmek WHERE filmek.film_ev = '" + req.body.bevitel1 + "',", function (err, rows, fields) { 
+if (err) throw err
+
+  console.log(rows)
+  
+  res.send(rows)
+})
+
+connection.end()
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
