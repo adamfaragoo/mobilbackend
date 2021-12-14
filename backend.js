831e7e8a658f2dbab42afd18329b046e0bbc544c
adamfaragoo <adam.farago004@gmail.com>
2021-12-14 10:19:24 +0100

    asd

--- backend.js
+++ backend.js
@@ -33,6 +33,28 @@ connection.query('SELECT * from filmek', function (err, rows, fields) {
 connection.end()
 })
 
+app.get('/mufajok', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'vizsgamunka'
+})
+
+connection.connect()
+
+connection.query('SELECT * from film_mufajok', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log(rows)
+  
+  res.send(rows)
+})
+
+connection.end()
+})
+
 app.post('/kereses', (req, res) => {
   var mysql = require('mysql')
   var connection = mysql.createConnection({
@@ -54,6 +76,32 @@ if (err) throw err
 connection.end()
 })
 
+
+//-------------------------------------------------------------------------------------------
+//Műfaj szerint szűri a filmeket
+//-------------------------------------------------------------------------------------------
+
+app.post('/filmszures', (req, res) => {
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'vizsgamunka'
+})
+
+connection.connect()
+
+connection.query('SELECT * from filmek WHERE film_mufaj ='+ req.body.bevitel2, function (err, rows, fields) {
+  if (err) throw err
+
+  console.log(rows)
+  
+  res.send(rows)
+})
+
+connection.end()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
